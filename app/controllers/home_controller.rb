1c8789e91a5acd108e2c15d3cacb65dbd2c8b4a0
umeyuki <umeyuki1326+github@gmail.com>
2017-12-12 17:22:05 +0900

    Change design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @zaiftoken_price = Rails.cache.fetch("zaiftoken_price", expires_in: 3.minutes ) do
-      res = HTTP.get("https://api.zaif.jp/api/1/last_price/zaif_jpy")
+    @bitcoin_price = Rails.cache.fetch("bitcoin_price", expires_in: 3.minutes ) do
+      res = HTTP.get("https://coincheck.com/api/ticker")
       JSON.parse(res.body)["last_price"]
+      JSON.parse(res.body)["last"].round.to_s(:delimited)
     end
   end
 end
