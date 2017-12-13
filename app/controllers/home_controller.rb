c40a31c3a6d8466dfa140c2f7193f96943f23e02
umeyuki <umeyuki1326+github@gmail.com>
2017-12-13 17:09:27 +0900

    WIP ripplenize

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @bitcoin_price = Rails.cache.fetch("bitcoin_price", expires_in: 3.minutes ) do
-      res = HTTP.get("https://coincheck.com/api/ticker")
-      JSON.parse(res.body)["last_price"]
-      JSON.parse(res.body)["last"].round.to_s(:delimited)
+    @xrp_price = Rails.cache.fetch("xrp_price", expires_in: 3.minutes ) do
+      res = HTTP.get("https://coincheck.com/api/rate/xrp_jpy")
+      JSON.parse(res.body)["rate"].round.to_s(:delimited)
     end
   end
 end
