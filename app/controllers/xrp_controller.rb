3179ea9407313ba049be0214e820d32a040aca1d
umeyuki <umeyuki1326+github@gmail.com>
2017-12-14 10:56:12 +0900

    Add zaif and xrp

--- app/controllers/xrp_controller.rb
+++ app/controllers/xrp_controller.rb
@@ -1,2 +1,13 @@
 class XrpController < ApplicationController
+    def index
+    @xrp_price = Rails.cache.fetch("xrp_price", expires_in: 3.minutes ) do
+      res = HTTP.get("https://coincheck.com/api/rate/xrp_jpy")
+      JSON.parse(res.body)["rate"].to_f.round.to_s(:delimited)
+    end
+
+    @bitcoin_price = Rails.cache.fetch("bitcoin_price", expires_in: 3.minutes ) do
+      res = HTTP.get("https://coincheck.com/api/ticker")
+      JSON.parse(res.body)["last"].round.to_s(:delimited)
+    end
+  end
 end
--- app/controllers/zaif_token_controller.rb
+++ app/controllers/zaif_token_controller.rb
@@ -1,2 +1,14 @@
 class ZaifTokenController < ApplicationController
+  def index
+    @bitcoin_price = Rails.cache.fetch("bitcoin_price", expires_in: 3.minutes ) do
+      res = HTTP.get("https://coincheck.com/api/ticker")
+      JSON.parse(res.body)["last_price"]
+      JSON.parse(res.body)["last"].round.to_s(:delimited)
+    end
+
+    @zaiftoken_price = Rails.cache.fetch("zaiftoken_price", expires_in: 3.minutes ) do
+      res = HTTP.get("https://api.zaif.jp/api/1/last_price/zaif_jpy")
+      JSON.parse(res.body)["last_price"]
+    end
+  end
 end
