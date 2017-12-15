36764f33d425950dc8f5ccdf2054b40060d37665
umeyuki <umeyuki1326+github@gmail.com>
2017-12-15 18:01:36 +0900

    Add font-awesome

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'bitcoin#index'
   resources :bitcoin, only: [:index]
+  resources :bitcoin_cash, only: [:index]
   resources :xrp, only: [:index]
   resources :zaif_token, only: [:index]
 end
